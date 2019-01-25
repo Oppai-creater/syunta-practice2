e1332d120e5e202c2cdf7083faf50a480e292d4f
Yama <souichirou19960210@gmail.comn>
2019-01-25 20:33:55 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: {
+      script: path.resolve(__dirname, 'assets/js/script.js'),
+    },
+    output: {
+      filename: 'js/[name].js',
+      path: path.resolve(__dirname, 'public')
+    },
+  },
+  {
+    mode: 'development',
+    entry: {
+      style: path.resolve(__dirname, 'assets/scss/style.scss'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'css/[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: './img/[name].[ext]',
+              outputPath: './',
+              publicPath: path => '.' + path
+            }
+          }
+        },
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        allChunks: true,
+        filename: "css/[name].css",
+      })
+    ],
+  }
+];
